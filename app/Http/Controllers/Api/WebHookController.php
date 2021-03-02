cca7d5ac6e45490dc85d7413430981b90e34fab3
Temitope Olotin <olotintemitope@gmail.com>
2021-03-02 15:40:38 +0100

    check for header

--- app/Http/Controllers/Api/WebHookController.php
+++ app/Http/Controllers/Api/WebHookController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 
 class WebHookController extends Controller
@@ -17,6 +18,14 @@ class WebHookController extends Controller
      */
     public function receive(Request $request): JsonResponse
     {
+        // check for headers
+        if (empty($request->header('X-Header-From')) ||
+            null === $request->header('X-Header-From')
+        ) {
+            Log::error('missing-header', $request->headers->all());
+
+            return response()->json([], Response::HTTP_BAD_REQUEST);
+        }
         Log::info('request-received', $request->all());
 
         return response()->json($request->all());
